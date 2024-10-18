9d42ba5f03d947f5c7ab6bd83befa4887fb94ac2
threeanra <trian.radis448@gmail.com>
2024-10-18 10:13:21 +0700

    Add : Add Guard in Master Tarif API

--- src/pasien/pasien.service.ts
+++ src/pasien/pasien.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Prisma, Pasien, EpisodePendaftaran, Pendaftaran } from '@prisma/client';
 import { formatISO } from 'date-fns';
--- src/setting/tarif/master-tarif.controller.ts
+++ src/setting/tarif/master-tarif.controller.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, UseGuards } from '@nestjs/common';
 import { MasterTarifService } from './master-tarif.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('master-tarif')
 export class MasterTarifController {
   constructor(private readonly masterTarifService: MasterTarifService) {}
