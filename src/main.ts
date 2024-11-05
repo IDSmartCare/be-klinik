01ed31b548b087f00f2413142d0e81160ebbe56b
threeanra <trian.radis448@gmail.com>
2024-11-05 16:41:56 +0700

    fix cors master tarif

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.HOST_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
   await app.listen(process.env.PORT);
 }
