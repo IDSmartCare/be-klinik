4b22edbddc8a197d4169a44705751f3e8f9a84fc
threeanra <trian.radis448@gmail.com>
2024-11-05 11:41:59 +0700

    fixing bug

--- src/cppt/dto/create-cppt.dto.ts
+++ src/cppt/dto/create-cppt.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export class CreateCpptDto {
     subjective: string
     objective: string
--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateJadwalDto, CreatePoliDto } from './dto/create-setting.dto';
 import { UpdateSettingDto } from './dto/update-setting.dto';
