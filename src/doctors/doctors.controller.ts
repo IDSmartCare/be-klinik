795de3a05ce9d21dc56c3e12479dca2712ebfead
Yahyan Setiadi <yahyan.nesya10@gmail.com>
2024-11-15 09:10:45 +0700

    Edit dokter

--- src/doctors/doctors.controller.ts
+++ src/doctors/doctors.controller.ts
@@ -12,6 +12,8 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateDokterDto } from './dto/create-dokter.dto';
 import { Doctors } from '@prisma/client';
 import { UpdateDoctorStatusDto } from './dto/update-doctors.dto';
+import { UpdatePoliKlinikDto } from './dto/update-poliKlinik.dto';
+import { UpdateDokterDto } from './dto/update-dokter.dto';
 @Controller('dokter')
 export class DoctorsController {
   constructor(private readonly doctorsService: DoctorsService) {}
@@ -36,5 +38,19 @@ export class DoctorsController {
   ) {
     const { isAktif } = updateDoctorStatusDto;
     return this.doctorsService.updateDoctorStatus(id, isAktif);
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch('/editdokter/:id')
+  async editDokter(
+    @Param('id') id: number,
+    @Body() updateDokterDto: UpdateDokterDto,
+    @Body('poliklinik') updatePoliKlinikDto: UpdatePoliKlinikDto,
+  ) {
+    return this.doctorsService.updateDokter(
+      id,
+      updateDokterDto,
+      updatePoliKlinikDto,
+    );
   }
 }
