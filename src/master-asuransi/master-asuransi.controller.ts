41f13c93eb2f9de093903b5fdbe3b08324141225
threeanra <trian.radis448@gmail.com>
2024-11-20 09:53:17 +0700

    FIX: fixing in master asuransi controller

--- src/master-asuransi/master-asuransi.controller.ts
+++ src/master-asuransi/master-asuransi.controller.ts
@@ -1,93 +1,81 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    HttpException,
-    HttpStatus,
-    NotFoundException,
-    Param,
-    ParseIntPipe,
-    Patch,
-    Post,
-    UseGuards,
-    ValidationPipe,
-  } from '@nestjs/common';
-  import { MasterAsuransiService } from './master-asuransi.service';
-  import { AuthGuard } from 'src/auth/auth.guard';
-  import { CreateMasterAsuransiDto } from './dto/create-master-asuransi.dto';
-  import { MasterAsuransi } from '@prisma/client';
-  import { UpdateMasterAsuransiDto } from './dto/update-master-asuransi.dto';
-  
-  @Controller('masterasuransi')
-  export class MasterAsuransiController {
-    constructor(private readonly masterAsuransiService: MasterAsuransiService) {}
-  
-    @UseGuards(AuthGuard)
-    @Get(':idFasyankes')
-    async listAsuransi(@Param('idFasyankes') idFasyankes: string) {
-      return this.masterAsuransiService.listAsuransi(idFasyankes);
-    }
-  
-    @UseGuards(AuthGuard)
-    @Post('/create')
-    async create(
-      @Body(new ValidationPipe())
-      createMasterAsuransiDto: CreateMasterAsuransiDto,
-    ) {
-      return this.masterAsuransiService.create(createMasterAsuransiDto);
-    }
-  
-    @UseGuards(AuthGuard)
-    @Patch('update/:id')
-    async updateAsuransi(
-      @Param('id', ParseIntPipe) id: number,
-      @Body() updateData: UpdateMasterAsuransiDto,
-    ): Promise<{ success: boolean; message: string; data: MasterAsuransi }> {
-      return this.masterAsuransiService.updateAsuransi(id, updateData) as Promise<{ success: boolean; message: string; data: MasterAsuransi }>;
-    }
-  
-    @UseGuards(AuthGuard)
-    @Delete('delete/:id')
-    async deleteAsuransi(
-      @Param('id') id: string,
-      @Body('idFasyankes') idFasyankes: string,
-    ): Promise<{ message: string; data?: any }> {
-      return this.masterAsuransiService.deleteAsuransi(id, idFasyankes);
-    }
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MasterAsuransiService } from './master-asuransi.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateMasterAsuransiDto } from './dto/create-master-asuransi.dto';
+import { MasterAsuransi } from '@prisma/client';
+import { UpdateMasterAsuransiDto } from './dto/update-master-asuransi.dto';
 
+@Controller('masterasuransi')
+export class MasterAsuransiController {
+  constructor(private readonly masterAsuransiService: MasterAsuransiService) {}
 
-    @Get(':id/:idFasyankes')
-    async findByIdWithResponse(
-      @Param('id') id: number,
-      @Param('idFasyankes') idFasyankes: string,
-    ) {
-      try {
-        const result = await this.prisma.masterAsuransi.findUnique({
-          where: {
-            id: Number(id),
-            idFasyankes: idFasyankes,
-          },
-        });
-  
-        if (!result) {
-          throw new NotFoundException({
-            success: false,
-            message: `Data dengan id ${id} dan idFasyankes ${idFasyankes} tidak ditemukan.`,
-          });
-        }
-  
-        return {
-          success: true,
-          message: 'Data berhasil ditemukan.',
-          data: result,
-        };
-      } catch (error) {
-        if (error instanceof NotFoundException) {
-          throw error;
-        }
-        console.error('Error fetching data:', error);
-        throw new Error('Terjadi kesalahan pada server.');
+  @UseGuards(AuthGuard)
+  @Get(':idFasyankes')
+  async listAsuransi(@Param('idFasyankes') idFasyankes: string) {
+    return this.masterAsuransiService.listAsuransi(idFasyankes);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('/create')
+  async create(
+    @Body(new ValidationPipe())
+    createMasterAsuransiDto: CreateMasterAsuransiDto,
+  ) {
+    return this.masterAsuransiService.create(createMasterAsuransiDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch('update/:id')
+  async updateAsuransi(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: UpdateMasterAsuransiDto,
+  ): Promise<{ success: boolean; message: string; data: MasterAsuransi }> {
+    return this.masterAsuransiService.updateAsuransi(
+      id,
+      updateData,
+    ) as Promise<{ success: boolean; message: string; data: MasterAsuransi }>;
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete('delete/:id')
+  async deleteAsuransi(
+    @Param('id') id: string,
+    @Body('idFasyankes') idFasyankes: string,
+  ): Promise<{ message: string; data?: any }> {
+    return this.masterAsuransiService.deleteAsuransi(id, idFasyankes);
+  }
+
+  @Get(':id/:idFasyankes')
+  async findByIdWithResponse(
+    @Param('id') id: number,
+    @Param('idFasyankes') idFasyankes: string,
+  ) {
+    try {
+      const result = await this.masterAsuransiService.findByIdWithResponse(
+        id,
+        idFasyankes,
+      );
+      return result;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
       }
+      console.error('Error fetching data:', error);
+      throw new Error('Terjadi kesalahan pada server.');
     }
   }
+}
