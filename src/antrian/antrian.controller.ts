9b6746a342e8bdc181034d15915673c8974d45f3
Yahyan Setiadi <yahyan.nesya10@gmail.com>
2024-11-25 14:28:48 +0700

    API store AtrianAdmisi

--- src/antrian/antrian.controller.ts
+++ src/antrian/antrian.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AntrianService } from './antrian.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateAntrianAdmisiDto } from './dto/create-admisi.dto';
 
 @Controller('antrian')
 export class AntrianController {
     constructor(private readonly antrianService: AntrianService) {}
 
-    
+    @UseGuards(AuthGuard)
+    @Post('/store-admisi')
+    async storeAntrianAdmisi(@Body() dto: CreateAntrianAdmisiDto) {
+      return await this.antrianService.storeAntrianAdmisi(dto);
+    }
 }
